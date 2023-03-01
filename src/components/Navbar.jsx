6ececf743a392c0dd4b467320399ac9bf2b6ab7a
Wanasak Suraintaranggoon <u510610433@gmail.com>
2023-03-01 22:26:30 +0700

    add layout, sidebar and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
 import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material'
+import {
+  ArrowDropDownOutlined,
   DarkModeOutlined,
   LightModeOutlined,
   Menu as MenuIcon,
   Search,
   SettingsOutlined,
 } from '@mui/icons-material'
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import FlexBetween from './FlexBetween'
-import { setMode } from '../state'
+import { setMode } from '../state/state'
+import profileImage from '../assets/profile.jpeg'
 
-const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch()
   const theme = useTheme()
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const isOpen = Boolean(anchorEl)
+  const handleClick = (event) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
 
   return (
     <AppBar
@@ -57,6 +75,66 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
           <IconButton>
             <SettingsOutlined sx={{ fontSize: '25px' }} />
           </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: 'flex',
+                gap: '1rem',
+                textTransform: 'none',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box
+                component='img'
+                alt='profile'
+                height='32px'
+                width='32px'
+                src={profileImage}
+                borderRadius='50%'
+                sx={{
+                  objectFit: 'cover',
+                }}
+              />
+              <Box textAlign='left'>
+                <Typography
+                  fontSize='0.85rem'
+                  fontWeight='bold'
+                  sx={{
+                    color: theme.palette.secondary[100],
+                  }}
+                >
+                  {user?.name}
+                </Typography>
+                <Typography
+                  fontSize='0.75rem'
+                  sx={{
+                    color: theme.palette.secondary[200],
+                  }}
+                >
+                  {user?.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: '25px',
+                }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+            >
+              <MenuItem>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
--- src/scenes/Layout/Layout.jsx
+++ src/scenes/Layout/Layout.jsx
@@ -3,20 +3,28 @@ import { Box, useMediaQuery } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import Sidebar from '../../components/Sidebar'
+import { useGetUserQuery } from '../../state/api'
+import { useSelector } from 'react-redux'
 
 const Layout = () => {
   const isNonMobile = useMediaQuery('(min-width: 600px)')
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
+  const userId = useSelector((state) => state.global.userId)
+  const { data } = useGetUserQuery(userId)
+
   return (
     <Box display={isNonMobile ? 'flex' : 'block'} height='100%' width='100%'>
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
         drawerWidth='250px'
       />
-      <Box>
+      <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
